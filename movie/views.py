6e7ef03faeef92b3a76f40ba02cb2bf9af487cbd
jpduquep <jpduquep@eafit.edu.co>
2023-02-09 11:55:12 -0500

    cambios en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def home(request):
-    return render(request,'home.html',{'name':'Greg Lim'})
+    return render(request,'home.html',{'name':'Juan Pablo Duque'})
